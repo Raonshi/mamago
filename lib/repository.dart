3d3dcd0dcf9c5ccae7b97b9b64860128635bdad1
홍순원 <swhong@rsupport.com>
2023-07-21 13:45:03 +0900

    중복된 번역 결과 정제

--- lib/repository.dart
+++ lib/repository.dart
@@ -5,7 +5,7 @@ import 'package:mamago/provider/translate/translate_state.dart';
 import 'package:http/http.dart';
 
 class GptRepository {
-  final String key = "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";
+  final String key = "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";
 
   GptRepository() {
     lgr.d("GptManager has created");
@@ -39,16 +39,31 @@ class GptRepository {
 
     Map<String, dynamic> json = (jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>);
     if (json.containsKey("choices")) {
+      lgr.d(json);
       List<dynamic> choices = json["choices"] as List<dynamic>;
-
-      lgr.d(choices);
-
-      return choices.map((e) => TranslateItem.fromJson(e as Map<String, dynamic>)).map((e) {
+      List<TranslateItem> translationItems =
+          choices.map((e) => TranslateItem.fromJson(e as Map<String, dynamic>)).map((e) {
         if ((e.text ?? " \n ").isNotEmpty) {
           return e.copyWith(text: e.text!.split("\n")[1]);
         }
         return e;
       }).toList();
+
+      List<TranslateItem> result = [];
+      for (int i = 0; i < translationItems.length; i++) {
+        bool isDuplicated = false;
+        for (int j = 0; j < result.length; j++) {
+          if (translationItems[i].text == result[j].text) {
+            isDuplicated = true;
+            break;
+          }
+        }
+        if (!isDuplicated) {
+          result.add(translationItems[i]);
+        }
+      }
+
+      return result;
     }
     return [];
   }
@@ -75,8 +90,10 @@ class GptRepository {
       "logprobs": null,
     };
     Response response = await post(uri, headers: headers, body: jsonEncode(body));
+
     if (response.statusCode != 200) {
-      throw Exception("Translation request has failed : ${response.statusCode}");
+      lgr.e(utf8.decode(response.bodyBytes));
+      throw Exception("Language detection request has failed : ${response.statusCode}");
     }
 
     Map<String, dynamic> json = (jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>);
